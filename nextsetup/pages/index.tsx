e013d46e6baa971aaabb86d1b805416b46bd8268
alessandervs <sanderpant1@hotmail.com>
2022-01-18 21:47:45 -0300

    Criação do projeto base

---
+++ nextsetup/pages/index.tsx
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+
+      <main >
+     <h1>Hello WOlrd!</h1>
+      </main>
+
+      <footer >
+
+      </footer>
+    </div>
+  )
+}
